6f84ab5328214a1b26e0b531e2c731c53032daaf
Johannes <johanness_charles@yahoo.com>
2019-08-13 11:38:13 +0300

    First commit

---
+++ app/src/main/java/com/example/travelmantics2019/Service/InstanceService.java
@@ -0,0 +1,8 @@
+package com.example.travelmantics2019.Service;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class InstanceService extends FirebaseMessagingService {
+
+}
